bff83292869e9c31d39d0e7f3ac3848c75d1c4c8
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-04-04 21:17:01 +0530

    bootstrap css added & env changes

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "../styles.css";
+import {API} from "../backend";
 
 const Home=()=>{
+    console.log("API is ",API);
     return(
         <div>
-            <h1>It's Home</h1>
+            <h1 className="text-white">It's Home</h1>
         </div>
     );
 }

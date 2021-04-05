e0ed8ecaf7b5e9aa52ff92d6129fe62756d85777
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-04-05 22:17:56 +0530

    signin & signup page setup

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -3,9 +3,30 @@ import Base from '../core/Base';
 
 
 const Signin=()=>{
+    const signInForm = () => {
+        return (
+          <div className="row">
+            <div className="col-md-6 offset-sm-3 text-left">
+              <form>
+                <div className="form-group">
+                  <label className="text-light">Email</label>
+                  <input className="form-control" type="email" />
+                </div>
+    
+                <div className="form-group">
+                  <label className="text-light">Password</label>
+                  <input className="form-control" type="password" />
+                </div>
+                <br/>
+                <button className="btn btn-success btn-block">Submit</button>
+              </form>
+            </div>
+          </div>
+        );
+      };
     return (
         <Base title="SignIn Page" description="Sign In Page here.">
-            <h1>SignIn works.</h1>
+            {signInForm()}
         </Base>
     )
 }
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -3,9 +3,35 @@ import Base from '../core/Base';
 
 
 const Signup=()=>{
+    const signUpForm = () => {
+        return (
+          <div className="row">
+            <div className="col-md-6 offset-sm-3 text-left">
+              <form>
+                <div className="form-group">
+                  <label className="text-light">Name</label>
+                  <input className="form-control" type="text" />
+                </div>
+                <div className="form-group">
+                  <label className="text-light">Email</label>
+                  <input className="form-control" type="email" />
+                </div>
+    
+                <div className="form-group">
+                  <label className="text-light">Password</label>
+                  <input className="form-control" type="password" />
+                </div>
+                <br/>
+                <button className="btn btn-success btn-block">Submit</button>
+              </form>
+            </div>
+          </div>
+        );
+      };
+
     return (
         <Base title="SignUp Page" description="Sign Up Page here.">
-            <h1>SignUp works.</h1>
+            {signUpForm()}
         </Base>
     )
 }

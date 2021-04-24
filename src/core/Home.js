284bc8fae34fbeb925f099acce314f64c1e7edff
Rupesh Kalra <rupeshkalra1999@gmail.com>
2021-04-24 23:28:14 +0530

    basic card setup

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,14 +2,14 @@ import React from "react";
 import "../styles.css";
 import {API} from "../backend";
 import Base from "./Base";
+import Card from "./Card";
 
 const Home=()=>{
-    console.log("API is ",API);
     return(
         <Base title="Home" description="Welcome To The Store.">
             <div className="row">
                 <div className="col-4">
-                    <button className="btn btn-success">Button</button>
+                <Card/>
                 </div>
                 <div className="col-4">
                     <button className="btn btn-success">Button</button>
